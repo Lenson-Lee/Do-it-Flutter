3a74d7ec97815cbfc8a5d69313859b69bd0a29e8
Lenson-Lee <dmstjs7437@naver.com>
2022-03-28 14:30:39 +0900

    [7. 카카오API 책 검색기능]

--- http_examle/lib/main.dart
+++ http_examle/lib/main.dart
@@ -26,57 +26,98 @@ class HttpApp extends StatefulWidget {
 class _HttpApp extends State<HttpApp> {
   String result = '';
   List? data;
+  TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
 
   @override
   void initState() {
     super.initState();
     data = new List.empty(growable: true);
+    _editingController = new TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http Example'),
+        title: TextField(
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: '검색어를 입력하세요.'),
+        ),
       ),
       body: Container(
         child: Center(
-            child: data!.length == 0 ? Text(
-                  '데이터가 없습니다.',
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
-            )
-                : ListView.builder(itemBuilder: (context, index) {
-              return Card(
-                child: Container(
-                  child: Column(
-
-    )
-              )
-              )
-        }
-        )
+            child: data!.length == 0
+                ? Text(
+                    '데이터가 없습니다.',
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  )
+                : ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Card(
+                          child: Container(
+                              child: Row(
+                        children: <Widget>[
+                          Image.network(
+                            data![index]['thumbnail'],
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.contain,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width - 150,
+                                child: Text(
+                                  data![index]['title'].toString(),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              Text(
+                                  '저자 : ${data![index]['authors'].toString()}'),
+                              Text(
+                                  '가격 : ${data![index]['sale_price'].toString()}'),
+                              Text(
+                                  '판매중 : ${data![index]['status'].toString()}'),
+                            ],
+                          )
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.start,
+                      )));
+                    },
+                    itemCount: data!.length,
+                  )),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          page = 1;
+          data!.clear();
+
           getJSONData();
         },
-        child: Icon(Icons.file_download),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons
+            .search), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 
   Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var url =
+        'https://dapi.kakao.com/v3/search/book?'
+        'target=title&page=$page&query=${_editingController!.value.text}';
     var response = await http.get(Uri.parse(url),
-        headers: { "Authorization": "KakaoAK 	8326c57a7bd585635fb25b1d5fd894bf"
-        });
+        headers: {"Authorization": "KakaoAK 8326c57a7bd585635fb25b1d5fd894bf"});
+    // print(response.body);
 
     setState(() {
       var dataConvertedToJSON = json.decode(response.body);
-      List result = dataConvertedToJSON['document'];
+      List result = dataConvertedToJSON['documents'];
       data!.addAll(result);
     });
-    return "Sucessfull";
+    return response.body;
   }
 }
